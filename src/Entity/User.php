31915d064a5d8b991a306cf5fcc9217f01c10397
Scott Collier <scott@onlinespaces.com>
2018-10-27 17:45:06 +0200

    Password must be at least 7 characters long and contain at least one digit, one upper case character, and one lower case letter and no spaces

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
@@ -32,6 +33,8 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      * @Groups("read")
+     * @Assert\NotBlank()
+     * @Assert\Length(min="3", max="180")
      */
     private $username;
 
@@ -43,17 +46,26 @@ class User implements UserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Assert\Regex(
+     *     pattern="/^(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{7,}$/",
+     *     message="Password must be at least 7 characters long and contain at least one digit, one upper case character, and one lower case letter."
+     * )
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups("read")
+     * @Assert\NotBlank()
+     * @Assert\Length(min="5", max="255")
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     * @Assert\Length(min="6", max="255")
      */
     private $email;
 
