3fca3ccdf2b82dcbd3f81862d81d7599c8e7dbf5
Scott Collier <scott.collier@umuc.edu>
2018-10-29 09:42:32 +0100

    Each slug must me unique

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -27,6 +28,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     }
  * )
  * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
+ * @UniqueEntity("slug", message="Each post must have a unique slug")
  */
 class BlogPost
 {
