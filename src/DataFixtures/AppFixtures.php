4528d02d0ba5fbb3498f66ec607de380f2b72683
Scott Collier <scott.collier@umuc.edu>
2018-10-30 11:45:55 +0100

    generate random tokens for users to confirm

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\BlogPost;
 use App\Entity\Comment;
 use App\Entity\User;
+use App\Security\TokenGenerator;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -18,10 +19,16 @@ class AppFixtures extends Fixture
     /** @var Factory $faker */
     private $faker;
 
-    public function __construct(UserPasswordEncoderInterface $encoder)
+    private $tokenGenerator;
+
+    public function __construct(
+        UserPasswordEncoderInterface $encoder,
+        TokenGenerator $tokenGenerator
+    )
     {
         $this->encoder = $encoder;
         $this->faker = Factory::create();
+        $this->tokenGenerator = $tokenGenerator;
     }
 
     /**
@@ -95,6 +102,11 @@ class AppFixtures extends Fixture
                 ->setPassword($this->encoder->encodePassword($user, "passWord1"));
 
             $user->setEnabled(true);
+
+            if(0 !== $i && 0 === $i % 4) {
+                $user->setEnabled(false);
+                $user->setConfirmationToken($this->tokenGenerator->getRandomSecureToken());
+            }
 
             if($roleidx >= count($roles)) {
                 $roleidx = 0;
